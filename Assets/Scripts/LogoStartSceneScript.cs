67b155c06f2dff85ef20eb7213ca8382aceb05a6
zciecielag <zuzannaciecielag1@gmail.com>
2024-10-27 12:55:26 +0100

    Logo start switches to choosing contrast menu
    After a couple seconds the logo start scene switches to the contrast choosing scene (needs fade out implementation).

---
+++ Assets/Scripts/LogoStartSceneScript.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+
+    [SerializeField]
+    private string sceneNameLoad;
+
+    void Start()
+    {
+        
+    }
+
+    void Awake()
+    {
+        //Important thing to remember: Starting a coroutine doesn't
+        //pause the whole script, it just waits inside the coroutine,
+        //so the next instruction has to be put INSIDE the IEnumerator \/
+        StartCoroutine(waiting());
+    }
+
+    void Update()
+    {
+        
+    }
+
+    IEnumerator waiting()
+    {
+        yield return new WaitForSeconds(4);
+        SceneManager.LoadScene(sceneName:sceneNameLoad);
+    }
+}
