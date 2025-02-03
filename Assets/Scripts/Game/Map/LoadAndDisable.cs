223a178df797e8d630d01e6cccb36f73896903d5
zciecielag <zuzannaciecielag1@gmail.com>
2025-02-03 17:57:20 +0100

    Bug fixes
    - Load on spawn points now only runs once
    - Szafka in Pantry has a working Audio Source
    - Improved main mechanic falling objects look

--- Assets/Scripts/Game/Map/LoadAndDisable.cs
+++ Assets/Scripts/Game/Map/LoadAndDisable.cs
@@ -10,10 +10,10 @@ public class LoadAndDisable : MonoBehaviour
     
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (this.gameObject.CompareTag(GameDataManager.Instance.GetGameData().playerSpawnPointTag))
+        if (this.gameObject.CompareTag(GameDataManager.Instance.GetGameData().playerSpawnPointTag) && canEnable)
         {
             GameDataManager.Instance.LoadGame();
+            canEnable = false;
         }
-        canEnable = false;
     }
 }
--- Assets/Scripts/MainMechanicPrototype/ObjectCollision.cs
+++ Assets/Scripts/MainMechanicPrototype/ObjectCollision.cs
@@ -86,7 +86,14 @@ public class ObjectCollision : MonoBehaviour
         if (tryAgainPanel != null)
         {
             tryAgainPanel.SetActive(true);
-            yield return new WaitForSeconds(5.0f);
+            Time.timeScale = 0f;
+            float pauseEndTime = Time.realtimeSinceStartup + 3.0f;
+            while (Time.realtimeSinceStartup < pauseEndTime)
+            {
+                yield return 0;
+            }
+            Time.timeScale = 1f;
+            //yield return new WaitForSeconds(5.0f);
             tryAgainPanel.SetActive(false);
         }
         
@@ -100,7 +107,14 @@ public class ObjectCollision : MonoBehaviour
         if (gameCompletedPanel != null)
         {
             gameCompletedPanel.SetActive(true);
-            yield return new WaitForSeconds(3.0f);
+            Time.timeScale = 0f;
+            float pauseEndTime = Time.realtimeSinceStartup + 3.0f;
+            while (Time.realtimeSinceStartup < pauseEndTime)
+            {
+                yield return 0;
+            }
+            Time.timeScale = 1f;
+            //yield return new WaitForSeconds(3.0f);
             gameCompletedPanel.SetActive(false);
         }
 
